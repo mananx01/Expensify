725f50539356c48b949972e07265ea7d1f854586
mananx01 <mananupmanyu@gmail.com>
2024-11-18 02:22:51 +0530

    deploy changes

--- app/(routes)/dashboard/payment-success/page.jsx
+++ app/(routes)/dashboard/payment-success/page.jsx
@@ -1,18 +1,38 @@
+"use client"
 import { CheckCircle } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
 
-function page({searchParams: { amount }}) {
+function Page() {
+  const [searchParams, setSearchParams] = useState(null)
+  const searchParamsFromUrl = useSearchParams()
+
+  useEffect(() => {
+    if (searchParamsFromUrl) {
+      setSearchParams({
+        amount: searchParamsFromUrl.get('amount'),
+        payment_intent: searchParamsFromUrl.get('payment_intent'),
+        payment_intent_client_secret: searchParamsFromUrl.get('payment_intent_client_secret'),
+        redirect_status: searchParamsFromUrl.get('redirect_status'),
+      })
+    }
+  }, [searchParamsFromUrl])
+
+  if (!searchParams) return <div>Loading...</div> // Show a loading state while waiting for searchParams
+
   return (
     <div className='font-bold text-3xl text-green-600'>
       <div>
-          <div className='font-bold text-green-950 text-2xl'>Amount: ${amount}</div>
-          <div className='flex items-center gap-3 '>
-              <h2 className='font-bold text-3xl text-green-600'>Payment Successful </h2>
-              <CheckCircle/>
-          </div>  
+        <div className='font-bold text-green-950 text-2xl'>Amount: ${searchParams.amount}</div>
+        <div className='font-bold text-green-950 text-xl'>Payment Intent: {searchParams.payment_intent}</div>
+        <div className='font-bold text-green-950 text-xl'>Redirect Status: {searchParams.redirect_status}</div>
+        <div className='flex items-center gap-3 '>
+          <h2 className='font-bold text-3xl text-green-600'>Payment Successful </h2>
+          <CheckCircle />
+        </div>
       </div>
     </div>
   )
 }
 
-export default page
+export default Page
