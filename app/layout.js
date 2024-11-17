ad45a6d01884e0a1b83ef24715b8fc8005c3197f
mananx01 <mananupmanyu@gmail.com>
2024-11-18 02:34:09 +0530

    deploy changes

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import {Inter, Outfit} from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const outfit = Outfit({subsets: ["latin"]})
 
